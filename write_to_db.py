b67d44911c722be30e866c4761f742bc2d40f331
tillewolle <35780949+tillewolle@users.noreply.github.com>
2023-07-10 13:54:10 +0200

    i

--- write_to_db.py
+++ write_to_db.py
@@ -1,6 +1,7 @@
 import csv
 import pymssql
 
+i = 0
 
 conn = pymssql.connect(server="127.0.0.1", user='root', password='root', database='pv_data')
 
@@ -12,10 +13,11 @@ csv_data = csv.reader(file)
 skipHeader = True
 
 for row in csv_data:
+
     if skipHeader:
         skipHeader = False
         continue
 #    cur.execute('INSERT pv-werte (Zeit, ETotal, PAC, PDC)' 'VALUES(%s, %s, %s, %s)' row)
-cur.execute('INSERT pv-werte (Zeit, ETotal, PAC, PDC)', 'VALUES(%s, %s, %s, %s)', row)
+cur.execute('INSERT pv-werte (Zeit, ETotal, PAC, PDC)', 'VALUES(i + 1,%s, %s, %s, %s)', row)
 cur.close()
 conn.close()
